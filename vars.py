05920257ccd2116894a7092a9e12186b1df069cb
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-03-26 12:36:37 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,10 +1,10 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+#BILLA TEAM 亗
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "26537149")
-API_HASH  = os.environ.get("API_HASH", "2697a63a384fd9b95520ee63530f6a75")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7318468475:AAEkx4eYifs7c52kTQ7eZX7TSy4F7rz7Ouw") 
+API_ID    = os.environ.get("API_ID", "24400704")
+API_HASH  = os.environ.get("API_HASH", "985be086326f373e026ea06e5fdef026")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7613208761:AAFqKi81Wd9h7fVqFZ5ODXpLJDNJNIcYTLo") 
 
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
